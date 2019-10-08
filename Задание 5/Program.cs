f847a95a79d3c59ef753c29d9eab5f883c9f5def
ya.zmeya.2001@gmail.com <ya.zmeya.2001@gmail.com>
2019-10-08 21:55:55 +0300

    Добавьте файлы проекта.

---
+++ Задание 5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Задание_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите  целочисленные катеты a и b");
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+            double S = a * b / 2;
+            double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            double P = a + b + c;
+            Console.WriteLine("S=" + S);
+            Console.WriteLine("P=" + P);
+        }
+    }
+}
